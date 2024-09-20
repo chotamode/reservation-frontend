4b704beb1c086a4e094c86f2790ec7f3f02962d8
Eduard <chotamode@gmail.com>
2024-09-20 17:51:54 +0200

    smol fixes

---
+++ src/components/CustomDropdown.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+function CustomDropdown({ options, selectedOptions, onChange }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleCheckboxChange = (option) => {
+        onChange(option);
+    };
+
+    return (
+        <div className="relative z-10 w-full">
+            <button
+                className="rounded-2xl p-2 px-4 drop-shadow-md w-full text-left bg-white border-1 border-gray-300"
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                Выбрать пратики
+            </button>
+            {isOpen && (
+                <div className="absolute bg-white border-1 border-gray-300 rounded-xl mt-2 w-full">
+                    {options.map((option) => (
+                        <label key={option} className="flex items-center p-2">
+                            <input
+                                type="checkbox"
+                                value={option}
+                                onChange={() => handleCheckboxChange(option)}
+                                checked={selectedOptions.includes(option)}
+                                className="mr-2"
+                            />
+                            {option}
+                        </label>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default CustomDropdown;
--- src/pages/ChoosePsychologist.jsx
+++ src/pages/ChoosePsychologist.jsx
@@ -1,12 +1,11 @@
-// src/pages/ChoosePsychologist.jsx
-import {useEffect, useState} from "react";
+import { useState } from "react";
 import TopNav from "../components/topnav/TopNav.jsx";
-import SearchIcon from "../assets/images/search.svg";
 import Footer from "../components/footer/Footer.jsx";
 import useFetchPsychologists from "../hooks/psychologist/useFetchPsychologists.js";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import AuthModal from "../components/AuthModal.jsx";
 import useAuth from "../hooks/useAuth.js";
+import CustomDropdown from "../components/CustomDropdown.jsx";
 
 function PsychologistCard2({ psychologist }) {
     const [isAuthModalOpen, setIsAuthModalOpen] = useState(false);
@@ -39,7 +38,7 @@ function PsychologistCard2({ psychologist }) {
                     {psychologist.mini_description}
                 </p>
                 <div className="flex flex-wrap gap-2 mb-10">
-                    {psychologist.specializations.map((practice, index) => (
+                    {psychologist.therapy_type.map((practice, index) => (
                         <span key={index} className="rounded-lg bg-[#D3DBA8] p-1 px-2 text-xs">
                             {practice}
                         </span>
@@ -57,46 +56,22 @@ function PsychologistCard2({ psychologist }) {
     );
 }
 
-const psychologists = [{
-    name: "Иван Иванов",
-    img: "https://xsgames.co/randomusers/assets/avatars/male/10.jpg",
-    speciality: "Клинический психолог",
-    description: "Иван Иванов — психолог с 10-летним стажем работы. Он специализируется на клинической психологии и помогает людям справиться с депрессией, тревожностью и другими психологическими проблемами.",
-    practices: ["КПТ", "Психоанализ", "Гештальт-терапия"]
-}, {
-    name: "Мария Петрова",
-    img: "https://xsgames.co/randomusers/assets/avatars/female/17.jpg",
-    speciality: "Психотерапевт",
-    description: "Мария Петрова — психотерапевт с 5-летним стажем работы. Она специализируется на психотерапии и помогает людям справиться с проблемами в отношениях, стрессом и другими психологическими проблемами.",
-    practices: ["КПТ", "Гештальт-терапия", "Гуманистическая психотерапия", "КПТ", "Гештальт-терапия", "Гуманистическая психотерапия", "КПТ", "Гештальт-терапия", "Гуманистическая психотерапия"]
-}, {
-    name: "Алексей Сидоров",
-    img: "https://xsgames.co/randomusers/assets/avatars/male/17.jpg",
-    speciality: "Когнитивный психолог",
-    description: "Алексей Сидоров — психолог с 7-летним стажем работы. Он специализируется на когнитивной психологии и помогает людям справиться с проблемами внимания, памяти и другими психологическими проблемами.",
-    practices: ["КПТ", "Психоанализ", "Гештальт-терапия"]
-}, {
-    name: "Елена Кузнецова",
-    img: "https://xsgames.co/randomusers/assets/avatars/female/25.jpg",
-    speciality: "Психолог-консультант",
-    description: "Елена Кузнецова — психолог с 3-летним стажем работы. Она специализируется на консультировании и помогает людям справиться с проблемами в работе, учебе и другими психологическими проблемами.",
-    practices: ["КПТ", "Психоанализ", "Гештальт-терапия"]
-}, {
-    name: "Анна Смирнова",
-    img: "https://xsgames.co/randomusers/assets/avatars/female/35.jpg",
-    speciality: "Психолог-практик",
-    description: "Анна Смирнова — психолог с 2-летним стажем работы. Она специализируется на практической психологии и помогает людям справиться с проблемами в поведении, эмоциях и другими психологическими проблемами.",
-    practices: ["КПТ", "Психоанализ", "Гештальт-терапия"]
-}, {
-    name: "Дмитрий Иванов",
-    img: "https://xsgames.co/randomusers/assets/avatars/male/71.jpg",
-    speciality: "Психолог-консультант",
-    description: "Дмитрий Иванов — психолог с 4-летним стажем работы. Он специализируется на консультировании и помогает людям справиться с проблемами в работе, учебе и другими психологическими проблемами.",
-    practices: ["КПТ", "Психоанализ", "Гештальт-терапия"]
-}];
-
 function ChoosePsychologist() {
     const { psychologists, loading, error } = useFetchPsychologists();
+    const [selectedPractices, setSelectedPractices] = useState([]);
+
+    const handleCheckboxChange = (practice) => {
+        setSelectedPractices((prevSelectedPractices) =>
+            prevSelectedPractices.includes(practice)
+                ? prevSelectedPractices.filter((p) => p !== practice)
+                : [...prevSelectedPractices, practice]
+        );
+    };
+
+    const filteredPsychologists = psychologists.filter((psychologist) =>
+        selectedPractices.length === 0 ||
+        psychologist.therapy_type.some((practice) => selectedPractices.includes(practice))
+    );
 
     return (
         <div>
@@ -105,20 +80,12 @@ function ChoosePsychologist() {
                 <h1 className="font-semibold font-[#39442B]">
                     Идеальный специалист для вас:
                 </h1>
-                {/* Search */}
-                <div className="relative">
-                    <input type="text" id="search" className="rounded-2xl p-2 px-4 drop-shadow-md w-full pr-10"
-                           placeholder="Поиск по запросу" />
-                    <img src={SearchIcon} alt="Search"
-                         className="absolute right-3 top-1/2 transform -translate-y-1/2 w-5 h-5" />
-                </div>
-                {/* Dropdown menu */}
-                <select name="psychologist" id="psychologist" className="rounded-xl p-2 border-1 border-gray-300 w-36">
-                    <option value="CBT">Когнитивно-поведенческая терапия</option>
-                    <option value="Psychoanalysis">Психоанализ</option>
-                    <option value="Gestalt">Гештальт-терапия</option>
-                    <option value="Humanistic">Гуманистическая психотерапия</option>
-                </select>
+                {/* Custom dropdown with checkboxes */}
+                <CustomDropdown
+                    options={["CBT", "psychoanalysis", "gestalt", "humanistic"]}
+                    selectedOptions={selectedPractices}
+                    onChange={handleCheckboxChange}
+                />
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-14">
@@ -127,7 +94,7 @@ function ChoosePsychologist() {
                 ) : error ? (
                     <div>Error: {error}</div>
                 ) : (
-                    psychologists.map(psychologist => (
+                    filteredPsychologists.map(psychologist => (
                         <PsychologistCard2 key={psychologist.name} psychologist={psychologist} />
                     ))
                 )}
